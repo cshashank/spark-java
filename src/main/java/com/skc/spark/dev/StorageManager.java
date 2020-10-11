5bd21cf495b99585848ffa25fba58a9cdc03f760
Shashank <chitnis.shashank@gmail.com>
2020-10-11 00:14:06 -0400

    Basic jar working in spark docker container
    Basic jar working in spark docker container

---
+++ src/main/java/com/skc/spark/dev/StorageManager.java
@@ -0,0 +1,13 @@
+package com.skc.spark.dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class StorageManager 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 123!" );
+    }
+}
