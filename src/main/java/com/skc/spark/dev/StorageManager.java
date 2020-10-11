c80a3a759228f3d5928b9f1dbbba0668cb2f0fdb
Shashank <chitnis.shashank@gmail.com>
2020-10-11 11:39:46 -0400

    success - read employee csv

--- src/main/java/com/skc/spark/dev/StorageManager.java
+++ src/main/java/com/skc/spark/dev/StorageManager.java
@@ -1,8 +1,9 @@
 package com.skc.spark.dev;
 import java.io.IOException;
-import java.net.InetSocketAddress;
 
-import com.sun.net.httpserver.*;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 /**
  * Hello world!
@@ -13,14 +14,15 @@ public class StorageManager
     public static void main( String[] args ) throws IOException{
         System.out.println( "Hello World 123!" );
 
+        SparkSession sparkSession = SparkSession.builder()
+        							.appName("Spark Files POC")
+        							.master("local[4]")
+        							.getOrCreate();
+        
+        Dataset<Row> df = sparkSession.read().csv("/opt/bitnami/spark/examples/jars/employee.csv");
+        df.show();
   
         new StorageHTTPServer().startHTTPServer();
- /*       
-        HttpServer server = HttpServer.create(new InetSocketAddress(8586), 0);
-        server.createContext("/test", new MyHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
-*/
     
     }
     
